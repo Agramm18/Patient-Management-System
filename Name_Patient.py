f27f7eb72d0cf1b8033c8c65298ec847b607e746
Alex Gramm <82889516+Agramm18@users.noreply.github.com>
2024-06-24 22:46:12 +0200

    Update Name_Patient.py

--- Name_Patient.py
+++ Name_Patient.py
@@ -1,45 +1,22 @@
-class DataPatient:
-
+class name_Patient:
     def __init__(self):
-        self.name = ""
-        self.last_name = ""
+        self.Name = ""
 
     def getName(self):
-        #Get the Patients first Name
-        self.name = input("Enter your first name: ")
-        return self.name
-
-    def getNameLast(self):
-        #Get the Patients last name
-        self.last_name = input("What is your last name: ")
-        return self.last_name
-
-    def __str__(self):
-        return f"The patient's name is: {self.name} {self.last_name}\n"
-
-    def errorHandlingFirstName(self):
-        #Error Handeling
         while True:
             try:
-                if self.getName() == "":
-                    print("Invalid input. Please enter your first name to continue.")
-                    continue
+                first = input("What is your first Name: ")
+                last = input("What is your last Name: ")
+
+                self.Name = f"{first} {last}"
+                print(f"Your Name is {self.Name}")
                 break
+
             except Exception as e:
+                print("Invalid input please follow the instructions to continue")
                 print(f"Error {e}")
 
-    def errorHandlingLastName(self):
-        #Error Handeling
-        while True:
-            try:
-                if self.getNameLast() == "":
-                    print("Invalid input. Please enter your last name to continue.")
-                    continue
-                break
-            except Exception as e:
-                print(f"Error {e}")
 
-NamePatient = DataPatient()
-NamePatient.errorHandlingFirstName()
-NamePatient.errorHandlingLastName()
-print(NamePatient)
+
+name = name_Patient()
+name.getName()
